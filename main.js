fb2b5b3ad3d115916abb8c83b91e19913f9a42a0
xiao-wu <3530388417@qq.com>
2026-03-29 14:41:43 +0800

    Clean rebuild without icon

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const { app, BrowserWindow } = require('electron');
-
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true
-        }
-    });
-    win.loadURL('https://xiao-wu990.github.io/wjcs');
-}
-
-app.whenReady().then(createWindow);
+const { app, BrowserWindow } = require('electron'); 
+ 
+function createWindow() { 
+    const win = new BrowserWindow({ 
+        width: 1200, 
+        height: 800, 
+        webPreferences: { 
+            nodeIntegration: false, 
+            contextIsolation: true 
+        } 
+    }); 
+    win.loadURL('https://xiao-wu990.github.io/wjcs'); 
+} 
+ 
+app.whenReady().then(createWindow); 
